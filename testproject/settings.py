2cd99b7fccb4ef3d9c62914fde858216e6d94459
umair2288 <umair2288@users.noreply.github.com>
2019-06-16 08:05:34 +0530

    allowed vm public ip

--- testproject/settings.py
+++ testproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1+y5bubp6_cz9xdo=c#7y$_3(87ay641$kr=i7b*n#ku2(nq&!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.67.49.110',]
 
 
 # Application definition
